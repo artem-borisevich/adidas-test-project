2ae0d6efedbe6b668d6a03dd89761625f1fc9269
aborisevich <artem.borisevich@idt.net>
2020-10-21 01:11:24 +0300

    fixed swagger

--- adidas-api/src/main/java/com/adidas/api/AdidasApiApplication.java
+++ adidas-api/src/main/java/com/adidas/api/AdidasApiApplication.java
@@ -1,7 +1,15 @@
 package com.adidas.api;
 
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class AdidasApiApplication {
@@ -10,4 +18,19 @@ public class AdidasApiApplication {
         SpringApplication.run(AdidasApiApplication.class, args);
     }
 
+    @Autowired
+    private RouteDefinitionLocator locator;
+
+    @Bean
+    public List<GroupedOpenApi> apis() {
+        List<GroupedOpenApi> groups = new ArrayList<>();
+        List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
+        definitions.stream()
+                .filter(routeDefinition -> routeDefinition.getId().matches(".*-service"))
+                .forEach(routeDefinition -> {
+                    String name = routeDefinition.getId().replaceAll("-service", "");
+                    GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").setGroup(name).build();
+                });
+        return groups;
+    }
 }
--- subscription-service/src/main/java/com/adidas/SubscriptionServiceApplication.java
+++ subscription-service/src/main/java/com/adidas/SubscriptionServiceApplication.java
@@ -1,8 +1,13 @@
 package com.adidas;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        servers = {@Server(url = "http://localhost:8080"), @Server(url = "http://subscription-service:8080")}
+)
 @SpringBootApplication
 public class SubscriptionServiceApplication {
 
